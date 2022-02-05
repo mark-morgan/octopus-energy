01f021719b839a0c6c6b46b6b6b05ddcfa1b72f1
Mark Morgan <github@mark-morgan.com>
2022-02-05 19:50:45 +0000

    added params for products->all

--- src/OctopusEnergy/Service/ProductService.php
+++ src/OctopusEnergy/Service/ProductService.php
@@ -4,20 +4,54 @@ namespace OctopusEnergy\Service;
 
 use OctopusEnergy\Product;
 use OctopusEnergy\Search;
+use OctopusEnergy\Util\Util;
 
 class ProductService extends AbstractService
 {
     /**
-     * Gets all Products as a ProductSearch object.
+     * Gets all Products as a Search object.
+     *
+     * @param bool|null $isVariable Fetch variable products.
+     * @param bool|null $isGreen Fetch green products.
+     * @param bool|null $isTracker Fetch tracker products.
+     * @param bool|null $isPrepay Fetch prepay products.
+     * @param bool|null $isBusiness Fetch business products.
+     * @param \DateTime|null $availableAt  Fetch products available at the specific DateTime.
+     * @param int $page Page to fetch, defaults to 1.
      *
      * @return Search
      */
-    public function all(): Search
+    public function all(bool $isVariable = null, bool $isGreen = null, bool $isTracker = null, bool $isPrepay = null, bool $isBusiness = null, \DateTime $availableAt = null, int $page = 1): Search
     {
+        $params = ['page' => $page];
+        if (null !== $isVariable) {
+            $params['is_variable'] = $isVariable;
+        }
+
+        if (null !== $isGreen) {
+            $params['is_green'] = $isGreen;
+        }
+
+        if (null !== $isTracker) {
+            $params['is_tracker'] = $isTracker;
+        }
+
+        if (null !== $isPrepay) {
+            $params['is_prepay'] = $isPrepay;
+        }
+
+        if (null !== $isBusiness) {
+            $params['is_business'] = $isBusiness;
+        }
+
+        if (null !== $availableAt) {
+            $params['available_at'] = Util::convertToAPIDateTime($availableAt);
+        }
+
         return $this->request(
             'get',
             '/v1/products/',
-            [],
+            $params,
             [
                 'type' => Search::class,
                 'nestedMap' => [
@@ -28,6 +62,8 @@ class ProductService extends AbstractService
     }
 
     /**
+     * Gets a specific product.
+     *
      * @param string $productCode
      *
      * @return Product
