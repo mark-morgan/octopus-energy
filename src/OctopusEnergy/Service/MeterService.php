9d6546d77236773e289d2871fdbf7186cf0f6e56
Mark Morgan <github@mark-morgan.com>
2022-02-05 19:28:00 +0000

    implemented more parameters on Consumption endpoint

--- src/OctopusEnergy/Service/MeterService.php
+++ src/OctopusEnergy/Service/MeterService.php
@@ -4,8 +4,10 @@ namespace OctopusEnergy\Service;
 
 use OctopusEnergy\Consumption;
 use OctopusEnergy\ElectricityMeter;
+use OctopusEnergy\Exception\InvalidArgumentException;
 use OctopusEnergy\GridSupplyPoints;
 use OctopusEnergy\Search;
+use OctopusEnergy\Util\Util;
 
 class MeterService extends AbstractService
 {
@@ -26,12 +28,41 @@ class MeterService extends AbstractService
         );
     }
 
-    public function getElectricityMeterConsumption(string $mpan, string $serialNumber)
+    public function getElectricityMeterConsumption(
+        string $mpan,
+        string $serialNumber,
+        int $page = 1,
+        \DateTime $periodFrom = null,
+        \DateTime $periodTo = null,
+        int $pageSize = 100
+    ): Search
     {
-        $val = $this->request(
+        if ($pageSize > 25000) {
+            throw new InvalidArgumentException('$pageSize cannot be greater than 25,000');
+        }
+
+        $params = [
+            'page' => $page,
+            'page_size' => $pageSize
+        ];
+        if (null !== $periodFrom) {
+            $params['period_from'] = Util::convertToAPIDateTime($periodFrom);
+        }
+
+        if (null !== $periodTo) {
+            if (null === $periodFrom) {
+                throw new InvalidArgumentException('$periodTo requires $periodFrom to be set, however it is currently null');
+            }
+            if ($periodTo < $periodFrom) {
+                throw new InvalidArgumentException('$periodTo must be greater than $periodFrom');
+            }
+            $params['period_to'] = Util::convertToAPIDateTime($periodTo);
+        }
+
+        return $this->request(
             'get',
             '/v1/electricity-meter-points/' . $mpan . '/meters/' . $serialNumber . '/consumption/',
-            [],
+            $params,
             [
                 'type' => Search::class,
                 'nestedMap' => [
@@ -39,8 +70,5 @@ class MeterService extends AbstractService
                 ]
             ]
         );
-
-        $done = true;
-        return $val;
     }
 }
